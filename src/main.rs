6f77dc2f061f9d14064ecea49eb46a2ee5aa2b9c
Titus Moore <tmoore@edgewebware.com>
2023-03-20 17:02:37 -0400

    More refactor of image

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use nokhwa::{
     nokhwa_initialize,
     pixel_format::{FormatDecoder, RgbFormat},
     query,
-    utils::{ApiBackend, RequestedFormat, RequestedFormatType, FrameFormat},
+    utils::{ApiBackend, FrameFormat, RequestedFormat, RequestedFormatType},
     Camera,
 };
 
@@ -18,14 +18,34 @@ fn main() {
     let camera_info = cameras.first().unwrap();
 
     let mut camera = Camera::new(camera_info.index().to_owned(), format).unwrap();
+    camera.set_frame_format(FrameFormat::RAWRGB).unwrap();
+
+    
     camera.open_stream().unwrap();
     let buffer = camera.frame().unwrap();
     camera.stop_stream().unwrap();
 
-    // Convert raw feed from frame to raw image
-    let raw_image = <RgbFormat as FormatDecoder>::write_output(FrameFormat::YUYV, buffer.resolution(), buffer.buffer()).unwrap();
-    image::save_buffer("img.png", &raw_image.to_owned(), buffer.resolution().width(), buffer.resolution().height(), image::ColorType::Rgb8).unwrap();
 
+    // println!("{:?}", buffer.buffer());
+
+    // Convert raw feed from frame to raw image
+    // let raw_image = <RgbFormat as FormatDecoder>::write_output(
+    //     FrameFormat::MJPEG,
+    //     buffer.resolution(),
+    //     buffer.buffer(),
+    // )
+    // .unwrap();
+
+    let decoded = buffer.decode_image::<RgbFormat>().unwrap();
+
+    image::save_buffer(
+        "img.png",
+        &decoded,
+        buffer.resolution().width(),
+        buffer.resolution().height(),
+        image::ColorType::Rgb8,
+    )
+    .unwrap();
 
     // match buffer.decode_image::<RgbFormat>() {
     //     Ok(img_buff) => {
