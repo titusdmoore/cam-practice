b20d967775d71d20448501b3dec28bec889e4fe5
Titus Moore <tmoore@edgewebware.com>
2023-03-20 16:12:17 -0400

    Refactored again, added new image

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
-use image::{ImageBuffer, Rgb};
 use nokhwa::{
     nokhwa_initialize,
-    pixel_format::{RgbAFormat, RgbFormat},
+    pixel_format::{FormatDecoder, RgbFormat},
     query,
-    utils::{ApiBackend, RequestedFormat, RequestedFormatType},
+    utils::{ApiBackend, RequestedFormat, RequestedFormatType, FrameFormat},
     Camera,
-    FormatDecoder
 };
-use std::{fs::File, io::Write};
 
 fn main() {
     nokhwa_initialize(|granted| {
@@ -25,9 +22,23 @@ fn main() {
     let buffer = camera.frame().unwrap();
     camera.stop_stream().unwrap();
 
-    
-    let mut vec_buff: Vec<u8> = Vec::new();
-    let image: ImageBuffer<Rgb<u8>, Vec<u8>> = buffer.decode_image::<RgbFormat>().unwrap();
+    // Convert raw feed from frame to raw image
+    let raw_image = <RgbFormat as FormatDecoder>::write_output(FrameFormat::YUYV, buffer.resolution(), buffer.buffer()).unwrap();
+    image::save_buffer("img.png", &raw_image.to_owned(), buffer.resolution().width(), buffer.resolution().height(), image::ColorType::Rgb8).unwrap();
 
-    image.save("image.png").expect("unable to save image");
+
+    // match buffer.decode_image::<RgbFormat>() {
+    //     Ok(img_buff) => {
+    //         let mut file = File::create("buffer.txt").unwrap();
+    //         file.write_all(&img_buff).unwrap();
+
+    //         let image: ImageBuffer<Rgb<u8>, Vec<u8>> = img_buff;
+
+    //         match image.save("image.png") {
+    //             Ok(_) => {}
+    //             Err(e) => println!("{}", e),
+    //         }
+    //     }
+    //     Err(e) => println!("{}", e),
+    // }
 }
